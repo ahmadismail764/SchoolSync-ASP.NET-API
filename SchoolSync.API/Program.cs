ab2cf7896b7f69a5ea37e102979c9dd07474960a
ahmadismail <ahmedeldeehy30305@gmail.com>
2025-08-14 15:13:33 +0300

    package version

--- SchoolSync.API/Program.cs
+++ SchoolSync.API/Program.cs
@@ -1,6 +1,7 @@
-
 using SchoolSync.Infra.Extensions;
 using SchoolSync.App.Extensions;
+using AutoMapper;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -8,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddAppServices();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly(), typeof(SchoolSync.App.MappingProfiles.UserProfile).Assembly);
 
 var app = builder.Build();
 
