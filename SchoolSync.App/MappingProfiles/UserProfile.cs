202ffe65ccc17258dcdefd502f6c6b511d62fd95
ahmadismail <ahmedeldeehy30305@gmail.com>
2025-09-01 13:25:13 +0300

    phone number field

--- SchoolSync.App/MappingProfiles/UserProfile.cs
+++ SchoolSync.App/MappingProfiles/UserProfile.cs
@@ -13,6 +13,7 @@ public class UserProfile : Profile
             .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
             .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
             .ForMember(dest => dest.SchoolId, opt => opt.MapFrom(src => src.SchoolId))
             .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId));
 
